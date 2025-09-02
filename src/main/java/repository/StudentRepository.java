6d6f892bee935a1aeea146666eae40c358297c35
Rushil Bansal <rushilbansal6000@gmail.com>
2025-09-02 14:08:43 +0530

    Delete src directory

--- src/main/java/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-                                                         //Jpa repository <entity class, primary key data type>
-}
